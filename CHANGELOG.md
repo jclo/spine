3658c45d6680afb253f1677c759beca6e9d0bc49
jclo <jclo@mobilabs.fr>
2021-11-12 15:30:40 +0100

    Released 0.90.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 ### HEAD
 
 
-### 0.0.0 (November 12, 2021)
+### 0.9.0 (November 12, 2021)
 
   * First build,
-  * ...,
+  * Released.
--- _dist/lib/spine.js
+++ _dist/lib/spine.js
@@ -1,5 +1,5 @@
 /*! ****************************************************************************
- * Spine v0.0.0-alpha.1
+ * Spine v0.9.0
  *
  * A tiny MVC framework inspired from Backbone.js.
  * (you can download it from npm or github repositories)
@@ -117,7 +117,7 @@
 
       // Useful to retrieve the library name and version when it is
       // embedded in another library as an object:
-      _library: { name: 'Spine', version: '0.0.0-alpha.1' },
+      _library: { name: 'Spine', version: '0.9.0' },
 
 
       // -- Private Static Methods ---------------------------------------------
@@ -264,7 +264,7 @@
 
     // Attaches constants to Spine that provide name and version of the lib.
     Spine.NAME = 'Spine';
-    Spine.VERSION = '0.0.0-alpha.1';
+    Spine.VERSION = '0.9.0';
 
 
     // -- Export
